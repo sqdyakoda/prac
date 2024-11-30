35f6016293c04ab2cd04bbe857866abf24884438
Airat <ajratsagdiev@mail.ru>
2024-11-30 15:04:19 +0300

    ...

--- homework/M2.c
+++ homework/M2.c
@@ -1,30 +1,29 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <string.h>
-#include <sys/wait.h>
+#include <stdio.h>
 #include <signal.h>
 typedef void (*pHandler) (int);
-static int count = 0;
+static int flag = 1;
 void onint(int sig) {
-    ++count;
-    printf("Hi!\n");
-}
-void onalrm(int sig) {
-    if (count > 1) {
+    if (flag) {
+        //если с последнего print(Hi!) успела пройти секунда
+        flag = 0;
+        alarm(1);
+        printf("Hi!\n");
+    } else {
         printf("Bye-bye!\n");
         exit(0);
     }
-    count = 0;
-    alarm(1);
+}
+void onalrm(int sig) {
+    flag = 1;
 }
 int main(void) {
     signal(SIGINT, onint);
     signal(SIGALRM, onalrm);
-    alarm(1);
+    // alarm(1);
     while(1)
         pause();
     return 0;
 }
+
--- homework/temp.c
+++ homework/temp.c
@@ -1,7 +1,25 @@
+#include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 int main() {
-    int a, b;
-    scanf("%d %d", &a, &b);
-    printf("the result is %d\n", a + b);
+    char str = 'a', s2;
+    int fd = open("inputt.txt", O_RDWR | O_TRUNC);
+    write(fd, &str, sizeof(char));
+    lseek(fd, SEEK_SET, 0);
+
+
+    read(fd, &s2, sizeof(char));
+    printf("[%c]\n", s2);
+
+
+    str = 'b';
+    write(fd, &str, sizeof(char));
+    read(fd, &s2, sizeof(char));
+    printf("[%c]\n", s2);
+
+    
     return 0;
 }
